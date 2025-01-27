5344d202a327760b492f6e329923ad07ca270797
limadijaya <limasuhendra@gmail.com>
2025-01-27 14:51:11 +0700

    init

---
+++ excercise-03.ts
@@ -0,0 +1,16 @@
+// Soal 3
+const cm: number = 150000;
+const km : number = cm / 100000;
+console.log("Hasil konversi cm ke km : " + km);
+console.log(`Hasil konversi cm ke km : ${km}`);
+
+
+// Soal 5
+let sentence:string = "Hello world";
+let search_string:string = "ell";
+console.log(sentence.replace(search_string, ""));
+
+
+
+
+
